59561fe3f69c3f8cde5f72add301f421b5cf9ffe
faelvilela <107723000+faelvilela@users.noreply.github.com>
2023-02-17 15:13:55 -0300

    V2

--- accScoreV2.py
+++ accScoreV2.py
@@ -3,18 +3,11 @@ from datetime import date, timedelta, datetime
 import os, shutil 
 import schedule
 
-print('Comecando...')
-ftp = FTP('host.ftp.com.br')  
-def conecta():
-    ftp.login(user='User', passwd='Pass') 
-    print('CONECTADO')
+global today
+today = datetime.now().strftime("%Y%m%d")
 
-def dw_ativos():
-    today = datetime.now().strftime("%Y%m%d")
-    local = 'C:/Ativos'
-    nome = 'AccountScore_'+today+'.csv'
+def baixar(nome, local):
     path = os.path.join('./', nome)
-    ftp.cwd('/Ativos_SA/Outbound')  
     try:
         with open(nome, 'wb') as fp:
             ftp.retrbinary('RETR AccountScore_'+today+'.csv', fp.write)
@@ -22,21 +15,26 @@ def dw_ativos():
     except error_perm:
         print('Erro ao encontrar: '+nome+' em '+local)
         os.remove(path)
-        
 
+print('Comecando...')
+ftp = FTP('host.ftp.com.br')  
+def conecta():
+    ftp.login(user='User', passwd='Pass') 
+    print('CONECTADO')
+
+def dw_ativos():
+    local = 'C:/Ativos'
+    nome = 'AccountScore_'+today+'.csv'
+    path = os.path.join('./', nome)
+    ftp.cwd('/Ativos_SA/Outbound')  
+    baixar(nome, local)
+        
 def dw_digio():
-    today = datetime.now().strftime("%Y%m%d")
     local = 'C:/Banco_Digio'
     nome = 'AccountScore_'+today+'.csv'
     path = os.path.join('./', nome)
     ftp.cwd('/Digio/Outbound') 
-    try: 
-        with open(nome, 'wb') as fp:
-            ftp.retrbinary('RETR AccountScore_'+today+'.csv', fp.write)
-        shutil.move(nome, local)
-    except error_perm:
-        print('Erro ao encontrar: '+nome+' em '+local)
-        os.remove(path)
+    baixar(nome, local)
 
 def job():
     conecta()
